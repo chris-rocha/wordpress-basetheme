ed38d9978c2f2d4c54dd89ac2335e8c9edc20867
Chris Rocha <chris.rocha@milespartnership.com>
2020-07-17 20:50:38 -0400

    min theme files

---
+++ wp-content/themes/mmgbase/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+	<?php
+
+	if ( have_posts() ) {
+
+		$i = 0;
+
+		while ( have_posts() ) {
+			$i++;
+			if ( $i > 1 ) {
+				echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
+			}
+			the_post();
+
+		}
+	}
+	?>
+
+	<?php //get_template_part( 'template-parts/pagination' ); ?>
+
+</main><!-- #site-content -->
+
+<?php
+get_footer();
