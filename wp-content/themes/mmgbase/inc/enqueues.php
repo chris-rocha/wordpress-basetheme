91fa1934ed2f7d997677c7e7be22389eb2352658
Chris Rocha <chris.rocha@milespartnership.com>
2020-07-19 16:43:49 -0400

    try rand version number

--- wp-content/themes/mmgbase/inc/enqueues.php
+++ wp-content/themes/mmgbase/inc/enqueues.php
@@ -3,7 +3,9 @@
  * Enqueue scripts and styles.
  */
 function mmgbase_scripts() {
-	wp_enqueue_style( 'mmgbase-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+	$rand = rand( 1, 99999999999 );
+
+	wp_enqueue_style( 'mmgbase-style', get_stylesheet_uri(), array(), $rand );
 
 	wp_register_style('googlefonts', 'https://fonts.googleapis.com/icon?family=Material+Icons' , false, null);
 	wp_enqueue_style('googlefonts');
@@ -17,7 +19,7 @@ function mmgbase_scripts() {
 	wp_register_script('vue-js', get_template_directory_uri() . '/js/vue.min.js', array(), null, true);
 	wp_enqueue_script('vue-js');
 
-	wp_register_script('site-js', get_template_directory_uri() . '/js/site.bundle.js', array('jquery'), null, true);
+	wp_register_script('site-js', get_template_directory_uri() . '/js/site.bundle.js', array('jquery'), $rand, true);
 	wp_enqueue_script('site-js');
 
 }
