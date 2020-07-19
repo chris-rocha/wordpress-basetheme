d94338fa4242f25306559623aad98ec5d14b170e
Chris Rocha <chris.rocha@milespartnership.com>
2020-07-18 23:31:21 -0400

    add menu image plugin, patch main menu css

--- wp-content/themes/mmgbase/inc/setup.php
+++ wp-content/themes/mmgbase/inc/setup.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'after_setup_theme', function(){
   add_theme_support( 'post-thumbnails' );
-  add_image_size( 'hero', 1920, 1080, array( 'center', 'center' ));
+	add_image_size( 'hero', 1920, 1080, array( 'center', 'center' ));
 });
 
 
